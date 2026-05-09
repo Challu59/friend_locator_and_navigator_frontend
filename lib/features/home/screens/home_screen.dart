e47b2ed9db9b14514eeaab063f84405d6ccb3aa7
challu <aayushadhikari72@gmail.com>
2026-05-09 21:21:51 +0545

    feat: add logout button to the home screen to clear all tokens and previous routes

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/features/auth/screens/login_screen.dart';
+import '../../../core/storage/token_storage.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
@@ -8,10 +10,30 @@ class HomeScreen extends StatefulWidget{
 }
 
 class _HomeScreenState extends State<HomeScreen>{
+
+  Future<void> logout(BuildContext context) async{
+    await TokenStorage.clearTokens();
+    Navigator.pushAndRemoveUntil(context,
+        MaterialPageRoute(
+          builder: (_) => LoginScreen(),
+        ),
+            (route) => false
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen"),),
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        actions: [
+          IconButton(
+          onPressed: () => logout(context),
+            icon: Icon(Icons.logout),
+            tooltip: 'LogOut',
+      ),
+        ],
+      ),
       body: Center(
         child: Text("Welcome"),
       ),
